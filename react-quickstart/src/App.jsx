c60cc82815999b07bf021b4041b529580c00f32f
Vhaugnn Diane Patino <vhaugnndianepatino@gmail.com>
2025-09-13 10:48:47 +0800

    Done writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -9,6 +9,16 @@ export default function MyApp(){
     <div>
       <h1>Welcome to my app</h1>
       <MyButton />
+      <AboutPage/>
     </div>
+  )
+}
+
+function AboutPage(){
+  return(
+    <>
+    <h1>About</h1>
+    <p>Hello there. <br />How do you do?</p>
+    </>
   )
 }
