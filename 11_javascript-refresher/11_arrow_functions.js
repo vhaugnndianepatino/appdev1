4085aaf39b3000b5c1f0b81d47a4674cfc0179cb
Vhaugnn Diane Patino <vhaugnndianepatino@gmail.com>
2025-09-23 22:57:46 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,9 @@
+const greet = (name) => "Hello, " + name;
+
+const square = (n) => n * n;
+
+const sayHi = () => console.log("Hi!");
+
+console.log(greet("Vhaugnn"));
+console.log(square(5));
+sayHi();
