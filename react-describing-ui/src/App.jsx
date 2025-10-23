60f2eb2a85c56f2c277857e05640f3bba9805b42
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-10-24 03:22:27 +0800

    Done with when and how to create multi-component files

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,19 +1,28 @@
-function Profile() {
-  return (
-    <img
-      className="profile-img"
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
+import Gallery from "./components/Gallery";  
+
+export default function App() {
+  return <Gallery />;
 }
-export default function Gallery() {
-  return (
-    <section>
-      <h1 className="title">Amazing scientists</h1>
-      <Profile /> 
-      <Profile />
-      <Profile />
-    </section>
-  );
-}
+
+
+// function Profile() {
+//   return (
+//     <img
+//       className="profile-img"
+//       src="https://i.imgur.com/MK3eW3As.jpg"
+//       alt="Katherine Johnson"
+//     />
+//   );
+// }
+// export default function Gallery() {
+//   return (
+//     <section>
+//       <h1 className="title">Amazing scientists</h1>
+//       <Profile /> 
+//       <Profile />
+//       <Profile />
+//     </section>
+//   );
+// } Task 1
+
+
---
+++ react-describing-ui/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+  return (
+    <img
+      className="profile-img"
+      src="https://i.imgur.com/QIrZWGIs.jpg"
+      alt="Alan L. Hart"
+    />
+  );
+}
