7cb07f637294d409e7ec1da9ad58ed6fb3cad979
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-10-24 02:39:16 +0800

    Done with how to write your first React component

---
+++ react-describing-ui/src/App.jsx
@@ -0,0 +1,19 @@
+function Profile() {
+  return (
+    <img
+      className="profile-img"
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
+export default function Gallery() {
+  return (
+    <section>
+      <h1 className="title">Amazing scientists</h1>
+      <Profile /> 
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
