7dc138b48a780e95cb686e0181247e78be2eaa5c
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-10-24 04:00:15 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,10 +1,30 @@
-import Gallery from "./components/Gallery";  
-
-export default function App() {
-  return <Gallery />;
+export default function TodoList() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+    </>
+  );
 }
 
 
+// TASK 2
+// import Gallery from "./components/Gallery";  
+
+// export default function App() {
+//   return <Gallery />;
+// } 
+
+// TASK 1
 // function Profile() {
 //   return (
 //     <img
@@ -23,6 +43,6 @@ export default function App() {
 //       <Profile />
 //     </section>
 //   );
-// } Task 1
+// } 
 
 
