2652c2aaf8459774d2aeac390e4b5122c7afa7a2
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-10-30 03:31:01 +0800

    Done with why understanding your UI as trees is useful

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -11,6 +11,19 @@ export default function TeaSet() {
     </>
   );
 }
+// function Cup({ guest }) {
+//   return <h2>Tea cup for guest #{guest}</h2>;
+// }
+
+// export default function TeaSet() {
+//   return (
+//     <>
+//       <Cup guest={1} />
+//       <Cup guest={2} />
+//       <Cup guest={3} />
+//     </>
+//   );
+// }
 
 
 
---
+++ react-describing-ui/src/components/RootComponent.jsx
@@ -0,0 +1,14 @@
+import ComponentA from "./ComponentA.jsx";
+import ComponentC from "./ComponentC.jsx";
+
+function RootComponent() {
+  return (
+    <div>
+      <h1>Root Component</h1>
+      <ComponentA />
+      <ComponentC />
+    </div>
+  );
+}
+
+export default RootComponent;
