de6e29487aec4556bb0eb142c741e83a89054d92
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-11-10 01:38:59 +0800

    Done with exporting and importing a component

--- react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/src/App.jsx
@@ -1,21 +1,10 @@
-function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
+import "./App.css";
+import Gallery from "./components/Gallery";
+
+function App() {
+  return <Gallery />;
 }
 
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  );
-}
+export default App;
 
 
