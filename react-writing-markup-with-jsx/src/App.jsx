36f9131f6858e3730200c5f86929938ef41239e6
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-11-10 01:48:02 +0800

    Done with converting HTML to JSX

---
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        class="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
