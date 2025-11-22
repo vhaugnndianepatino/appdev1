01d0a084190d0a85077fa4c4929290d966e4330f
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-11-22 10:44:08 +0800

    Done with more fun with JavaScript objects and curly braces

--- react-javascript-in-jsx-with-curly-braces/src/main.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import TodoList from './components/task3.jsx' 
+import TodoList from './components/task4.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
