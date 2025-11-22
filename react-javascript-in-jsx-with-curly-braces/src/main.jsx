c628a1a5af66ebecf5dbad3a584cda3f0381c7d9
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-11-22 10:27:26 +0800

    Done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/main.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import TodoList, { DateTodo } from './components/task2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TodoList />
+    <DateTodo weekdayLabel="Wednesday" />
   </StrictMode>,
 )
