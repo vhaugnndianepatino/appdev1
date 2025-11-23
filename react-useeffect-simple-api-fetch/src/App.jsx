098e4cfc5948636787082d05c71323a0badbbd68
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-11-23 22:02:38 +0800

    Done with fetching user data exercise

---
+++ react-useeffect-simple-api-fetch/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UsersFetch from "./UsersFetch";
+
+function App() {
+  return (
+    <>
+      <UsersFetch />
+    </>
+  );
+}
+
+export default App;
---
+++ react-useeffect-simple-api-fetch/src/UsersFetch.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+function UsersFetch() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users?_limit=5")
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("Error fetching todos:", error);
+        setLoading(false);
+      });
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+
+    return (
+        <div>
+            <h2>Users and Emails</h2>
+            <ul>
+                {users.map(user => (
+                <li key={user.id}>
+                    {user.name} - {user.email}
+                </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default UsersFetch;
